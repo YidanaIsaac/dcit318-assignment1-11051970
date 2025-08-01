19cda3acfe76d84c970a19eacbd7cdfa25eb2caa
YIDANA <macbookpro@YIDANAs-MacBook-Pro.local>
2025-08-01 23:22:35 +0000

    Added Ticket Price Calculator program

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        double price = 10.0;
+
+        if (age <= 12 || age >= 65)
+            price = 7.0;
+
+        Console.WriteLine($"Ticket Price: GHC{price}");
+    }
+}
