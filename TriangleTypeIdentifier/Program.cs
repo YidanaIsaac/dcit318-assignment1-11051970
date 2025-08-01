6ead02bfa4e4a9e51fbbfe62ccd982363473d62e
YIDANA <macbookpro@YIDANAs-MacBook-Pro.local>
2025-08-01 23:22:38 +0000

    Added Triangle Type Identifier program

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main()
+    {
+        Console.Write("Enter side 1: ");
+        double side1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter side 2: ");
+        double side2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter side 3: ");
+        double side3 = double.Parse(Console.ReadLine());
+
+        if (side1 == side2 && side2 == side3)
+            Console.WriteLine("Equilateral Triangle");
+        else if (side1 == side2 || side1 == side3 || side2 == side3)
+            Console.WriteLine("Isosceles Triangle");
+        else
+            Console.WriteLine("Scalene Triangle");
+    }
+}
